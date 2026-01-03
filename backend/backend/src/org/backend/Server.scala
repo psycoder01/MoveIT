2470a05c7875ad7037fdd555f432f6fbfb53855b
psycoder01 <aakashchy13@gmail.com>
2026-01-03 22:04:44 +0000

    feat: initialize the backend server

--- backend/backend/src/org/backend/Server.scala
+++ backend/backend/src/org/backend/Server.scala
@@ -1,7 +1,30 @@
 package org.backend
 
 import cats.effect._
+import cats.syntax.all._
+
+import org.typelevel.vault.Vault
+
+import fs2.io.net.Network
+import com.comcast.ip4s._
+
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.HttpRoutes
+import org.http4s.ember.server.EmberServerBuilder
 
 object Server extends IOApp.Simple:
-  def run: IO[Unit] =
-    IO.println("This is the backend server")
+  val routes = HttpRoutes.of[IO] { case _ =>
+    Ok("The server is running")
+  }
+  val httpApp = routes.orNotFound
+
+  def run =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(httpApp)
+      .build
+      .use(_ => IO.never)
--- backend/build.sc
+++ backend/build.sc
@@ -2,10 +2,15 @@ import mill._
 import scalalib._
 
 trait Base extends ScalaModule {
-  val scalaVersion = "3.7.1"
+  val scalaVersion  = "3.7.1"
+  val catsVersion   = "3.6.3"
+  val http4sVersion = "0.23.23"
 
   def ivyDeps = Agg(
-    ivy"org.typelevel::cats-effect:3.6.3"
+    ivy"org.typelevel::cats-effect:$catsVersion",
+    ivy"org.http4s::http4s-ember-server:$http4sVersion",
+    ivy"org.http4s::http4s-ember-client:$http4sVersion",
+    ivy"org.http4s::http4s-dsl:$http4sVersion"
   )
 }
 
