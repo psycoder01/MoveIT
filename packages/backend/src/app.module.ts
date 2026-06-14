849e19d9d3319c5f30e1f676220d2bbfc36682b5
psycoder01 <aakashchy13@gmail.com>
2026-06-14 20:50:54 +0100

    feat: add auth apis to the main module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 
 import envModule from "src/configs/env/env.module";
 import { DatabaseModule } from "src/database/database.module";
+
+import { AuthModule } from "src/auth/auth.module";
 import { OrganizationsModule } from "src/organizations/organizations.module";
 
 @Module({
-  imports: [envModule, DatabaseModule, OrganizationsModule],
+  imports: [envModule, DatabaseModule, AuthModule, OrganizationsModule],
 })
 export class AppModule {}
