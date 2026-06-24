1f3ef387b3b43cbd2bb1057653d5550f80c402ab
psycoder01 <aakashchy13@gmail.com>
2026-06-24 20:22:14 +0100

    fix: add cors for dev and prod

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -16,6 +16,13 @@ async function bootstrap() {
     defaultVersion: "1",
   });
 
+  const isProd = process.env.NODE_ENV === "prod";
+
+  app.enableCors({
+    origin: isProd ? process.env.FRONTEND_URL : "http://localhost:3000",
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
